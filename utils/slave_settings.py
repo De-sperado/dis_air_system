571e14c99f023ba88a56d04de3822e1edc6f4c69
ForgetfulJoy <jiayueliu@bupt.edu.cn>
2020-05-30 16:54:10 +0800

    update slave_settings

--- utils/slave_settings.py
+++ utils/slave_settings.py
@@ -6,5 +6,18 @@ RUNNING = 3
 WAITING = 4
 READY = 5
 
+'''从控机属性设置'''
+TEMP_LOW_BOUND = 10
+TEMP_HIGH_BOUND = 40
+
+COOL_MODE = 1
+WARM_MODE = 2
+DRY_MODE = 3
+
+MAX_SPEED = 7
+MIN_SPEED = 1
+
+FEE_RATE_PER_MIN = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+
 '''房间号'''
 room_ids = ['u101', 'u102', 'b103', 'b104', 't105', 't106', 'u201', 'b202', 't203']
